0003aac8dfb3d6086821c7bbe3a90865ddae215a
cabrera-evil <douglascabrera.dev@gmail.com>
2023-03-29 12:27:51 -0600

    Add: CORS

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as cors from 'cors';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(),
+  );
+
+  app.use(cors());
 
   await app.listen(process.env.PORT || 3000);
 }
