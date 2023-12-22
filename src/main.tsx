26710cff7272838d0cb9b0af81310d7bee847b00
cabrera-evil <douglascabrera.dev@gmail.com>
2023-12-21 20:45:44 -0600

    Feat: vite url

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { AuthProvider } from './context/AuthContext'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './context/UserContext'
 
-axios.defaults.baseURL = import.meta.env.VITE_API || 'https://todo-api.cabrera-dev.com/api'
+axios.defaults.baseURL = import.meta.env.VITE_API || 'https://localhost:3000/api'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
